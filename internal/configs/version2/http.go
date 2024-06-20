6a79cff417cc29d1e8f2ad30255d4bbf456baf4d
tomer pasman <pasmant@gmail.com>
2024-06-20 09:47:19 +0300

    Add allowlist feature to dos

--- internal/configs/version2/http.go
+++ internal/configs/version2/http.go
@@ -150,6 +150,7 @@ type WAF struct {
 type Dos struct {
 	Enable                 string
 	Name                   string
+	AllowListPath          string
 	ApDosPolicy            string
 	ApDosSecurityLogEnable bool
 	ApDosLogConf           string
